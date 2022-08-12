41f2975da615f315e86bc77ede180f5ecadb8499
aliciarojovera <aliciarojovera@hotmail.com>
2022-08-12 11:14:49 +0200

    finish course

--- goproject/cmd/cli/main.go
+++ goproject/cmd/cli/main.go
@@ -1,6 +1,11 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"github.com/aliciarojovera/beginners-guide-to-go/goproject/internal/network"
+
+)
 
 func main() {
 	fmt.Println("our app entrypoint")
+	network.Ping("127.0.0.1")
 }
